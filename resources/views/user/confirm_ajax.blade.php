5090dd4b9bcca51ebeac10e5b49112a429a1806a
Chandra140104 <chandrabgs140104@gmail.com>
2024-10-22 18:02:01 +0700

    JS 6 - Praktikum 3

--- resources/views/user/confirm_ajax.blade.php
+++ resources/views/user/confirm_ajax.blade.php
@@ -1,56 +1,52 @@
-<form action="{{ url('/user/ajax') }}" method="POST" id="form-tambah"> 
-    @csrf 
+@empty($user) 
     <div id="modal-master" class="modal-dialog modal-lg" role="document"> 
         <div class="modal-content"> 
             <div class="modal-header"> 
-                <h5 class="modal-title" id="exampleModalLabel">Tambah Data User</h5> 
+                <h5 class="modal-title" id="exampleModalLabel">Kesalahan</h5> 
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button> 
             </div> 
             <div class="modal-body"> 
-                <div class="form-group"> 
-                    <label>Level Pengguna</label> 
-                    <select name="level_id" id="level_id" class="form-control" required> 
-                        <option value="">- Pilih Level -</option> 
-                        @foreach($level as $l) 
-                            <option value="{{ $l->level_id }}">{{ $l->level_nama }}</option> 
-                        @endforeach 
-                    </select> 
-                    <small id="error-level_id" class="error-text form-text text-danger"></small> 
-                </div> 
-                <div class="form-group"> 
-                    <label>Username</label> 
-                    <input value="" type="text" name="username" id="username" class="form-control" required> 
-                    <small id="error-username" class="error-text form-text text-danger"></small> 
-                </div> 
-                <div class="form-group"> 
-                    <label>Nama</label> 
-                    <input value="" type="text" name="nama" id="nama" class="form-control" required> 
-                    <small id="error-nama" class="error-text form-text text-danger"></small> 
-                </div> 
-                <div class="form-group"> 
-                    <label>Password</label> 
-                    <input value="" type="password" name="password" id="password" class="form-control" required> 
-                    <small id="error-password" class="error-text form-text text-danger"></small> 
-                </div> 
-            </div>
-            <div class="modal-footer"> 
-                <button type="button" data-dismiss="modal" class="btn btn-warning">Batal</button> 
-                <button type="submit" class="btn btn-primary">Simpan</button> 
+                <div class="alert alert-danger"> 
+                    <h5><i class="icon fas fa-ban"></i> Kesalahan!!!</h5> 
+                    Data yang anda cari tidak ditemukan</div> 
+                <a href="{{ url('/user') }}" class="btn btn-warning">Kembali</a> 
             </div> 
         </div> 
     </div> 
-</form> 
+@else 
+    <form action="{{ url('/user/' . $user->user_id.'/delete_ajax') }}" method="POST" id="form-delete"> 
+    @csrf 
+    @method('DELETE') 
+    <div id="modal-master" class="modal-dialog modal-lg" role="document"> 
+        <div class="modal-content"> 
+            <div class="modal-header"> 
+                <h5 class="modal-title" id="exampleModalLabel">Hapus Data User</h5> 
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button> 
+            </div> 
+            <div class="modal-body"> 
+                <div class="alert alert-warning"> 
+                    <h5><i class="icon fas fa-ban"></i> Konfirmasi !!!</h5> 
+                    Apakah Anda ingin menghapus data seperti di bawah ini? 
+                </div> 
+                <table class="table table-sm table-bordered table-striped"> 
+                    <tr><th class="text-right col-3">Level Pengguna :</th><td class="col-9">{{ $user->level->level_nama }}</td></tr> 
+                    <tr><th class="text-right col-3">Username :</th><td class="col-9">{{ $user->username }}</td></tr> 
+                    <tr><th class="text-right col-3">Nama :</th><td class="col-9">{{ $user->nama }}</td></tr> 
+                </table> 
+            </div> 
+            <div class="modal-footer"> 
+                <button type="button" data-dismiss="modal" class="btn btn-warning">Batal</button> 
+                <button type="submit" class="btn btn-primary">Ya, Hapus</button> 
+            </div> 
+        </div> 
+    </div> 
+    </form> 
     <script> 
         $(document).ready(function() { 
-            $("#form-tambah").validate({ 
-                rules: { 
-                    level_id: {required: true, number: true}, 
-                    username: {required: true, minlength: 3, maxlength: 20}, 
-                    nama: {required: true, minlength: 3, maxlength: 100}, 
-                    password: {required: true, minlength: 6, maxlength: 20} 
-                }, 
+            $("#form-delete").validate({ 
+                rules: {}, 
                 submitHandler: function(form) { 
-                    $.ajax({ 
+                    $.ajax({
                         url: form.action, 
                         type: form.method, 
                         data: $(form).serialize(), 
@@ -91,4 +87,5 @@
                 } 
             }); 
         }); 
-    </script>
+    </script> 
+@endempty
